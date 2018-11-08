12c30f446947cd9d9b6eb197ec6a8a0ae8d8369b
EdTheLoon <devilred101@gmail.com>
2018-11-08 21:09:19 +0000

    Added timestamping to logging

--- server/log.go
+++ server/log.go
@@ -2,14 +2,16 @@ package server
 
 import (
 	"fmt"
+	"time"
 )
 
 // Log a message to console and log file
 func (s *server) Log(msg string) {
+	newMsg := "[" + time.Now().Format(time.UnixDate) + "] " + msg
 	// Print to console
-	fmt.Println(msg)
+	fmt.Println(newMsg)
 	// Append to log file
-	s.log.WriteString(msg + "\n")
+	s.log.WriteString(newMsg + "\n")
 }
 
 func (s *server) CloseLog() {
