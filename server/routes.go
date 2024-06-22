1738336d23e79bf1927805a2383dd0dd130f8072
Adrian Reid <devilred101@gmail.com>
2024-06-22 02:08:33 +0100

    Added additional key handling

--- server/routes.go
+++ server/routes.go
@@ -9,8 +9,7 @@ func (s *Server) routes() {
 	// Web Interface handling
 	s.router.HandleFunc("/", s.viewHandler)
 	s.router.HandleFunc("/web/", s.viewHandler)
-	s.router.HandleFunc("/web/admin/{page}", s.adminOnly(s.viewHandler))    // Handles showing all front end HTML pages
-	s.router.HandleFunc("/keys/{id}", s.adminOnly(s.getKey)).Methods("GET") // modifykey.html
+	s.router.HandleFunc("/web/admin/{page}", s.adminOnly(s.viewHandler)) // Handles showing all front end HTML pages
 
 	// Handles logging in or out
 	s.router.HandleFunc("/login", s.loginHandler)
@@ -19,8 +18,11 @@ func (s *Server) routes() {
 	s.router.HandleFunc("/admin", s.adminOnly(s.admin))
 
 	// Key Handlers
-	s.router.HandleFunc("/keys/add", s.adminOnly(s.addKey)).Methods("POST") // When key is added in addkey.html
-	s.router.HandleFunc("/keys/delete/{id}", s.adminOnly(s.deleteKey))      // when 'delete' is clicked on keys.html or modifykeys.html
+	s.router.HandleFunc("/keys/add", s.adminOnly(s.addKey)).Methods("POST")            // When key is added in addkey.html
+	s.router.HandleFunc("/keys/delete/{id}", s.adminOnly(s.deleteKey))                 // when 'delete' is clicked on keys.html or modifykeys.html
+	s.router.HandleFunc("/keys/{id}", s.adminOnly(s.getKey)).Methods("GET")            // Show the modify key page
+	s.router.HandleFunc("/keys/update/{id}", s.adminOnly(s.updateKey)).Methods("POST") // Update a key
+	s.router.HandleFunc("/keys/list", s.adminOnly(s.getAllKeys))                       // List all keys
 
 	// Door lock handlers
 	s.router.HandleFunc("/door/lock", s.adminOnly(s.lockDoor))
